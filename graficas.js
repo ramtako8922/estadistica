c6205a76e72ecb9d00c3377b255773a23783b0db
richi <rmc892020qgmail.com>
2025-06-16 00:00:28 -0500

    new version

--- graficas.js
+++ graficas.js
@@ -5,23 +5,20 @@ const height = 600; // Alto del lienzo
 const chartJSNodeCanvas = new ChartJSNodeCanvas({ width, height });
 
 async function crearGrafica(nombreArchivo, labels, data) {
-    const configuration = {
-        type: 'bar',
-        data: {
-            labels,
-            datasets: [{
-                label: 'Datos de ejemplo',
-                data,
-                
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                
-            }]
-        },
-       
-    };
+  const configuration = {
+    type: 'bar',
+    data: {
+      labels,
+      datasets: [{
+        label: 'Datos',
+        data,
+        backgroundColor: 'rgba(75, 192, 192, 0.6)'
+      }]
+    }
+  };
 
-    const imagen = await chartJSNodeCanvas.renderToBuffer(configuration);
-    fs.writeFileSync(nombreArchivo, imagen);
+  const image = await chartJSNodeCanvas.renderToBuffer(configuration);
+  fs.writeFileSync(nombreArchivo, image);
 }
 
 module.exports = {
