85db3ab27d6c1dde68693fd8506d8bf6c86fcaf9
Ricardo Mejía <67653677+ramtako8922@users.noreply.github.com>
2025-06-15 23:16:36 -0500

    Update estadisticas.js

--- estadisticas.js
+++ estadisticas.js
@@ -1,11 +1,12 @@
 function promedio(arr) {
+    If (!Array.isArray(arr) || arr.length===0 // Evitar división por cero
     return arr.reduce((a, b) => a + b, 0) / arr.length;
     
 }
 
 function desviacionEstandar(arr) {
     const media = promedio(arr);
-    if (arr.length === 0) return 0; // Evitar división por cero
+    
     const sumaCuadrados = arr.reduce((acc, val) => acc + Math.pow(val - media, 2), 0);
     return Math.sqrt(sumaCuadrados / (arr.length ));
     
